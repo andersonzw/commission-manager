53443899aad2861d68ea1fd48a833b25a966f35a
Anderson <84068667+andersonzw@users.noreply.github.com>
2023-12-14 19:05:54 +1100

    very basic layouting

---
+++ src/components/commissions/Commissions.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Commissions.css";
+const Commissions = () => {
+  return (
+    <section className=" inner-paddings commissions-section">
+      <div className="commission-card">
+        a
+      </div>
+    </section>
+  );
+};
+
+export default Commissions;
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,18 @@
+import "./Sidebar.css";
+const Sidebar = () => {
+  return (
+    <section className="inner-paddings sidebar-section">
+      <h1>Work in Progress</h1>
+      <ul>
+        <l1>Com1</l1>
+        <l1>Com2</l1>
+        <l1>Com3</l1>
+      </ul>
+      <h1>Completed</h1>
+      <h1>Not Accepted</h1>
+      <h1>Terminated</h1>
+    </section>
+  );
+};
+
+export default Sidebar;
